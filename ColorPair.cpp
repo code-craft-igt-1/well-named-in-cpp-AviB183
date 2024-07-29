6298d0678254c366cbe0d32f5c9f264363c994d7
Avinash Bhaskar Bhat <AvinashBhaskar.Bhat@philips.com>
2024-07-29 17:27:50 +0530

    Added class for color pair

---
+++ ColorPair.cpp
@@ -0,0 +1,43 @@
+#include "ColorPair.h"
+
+namespace TelCoColorCoder
+{
+	const char* MajorColorNames[MAX_MAJOR_COLOR] =
+	{
+		"White",
+		"Red",
+		"Black",
+		"Yellow",
+		"Violet"
+	};
+
+	const char* MinorColorNames[MAX_MINOR_COLOR] =
+	{
+		"Blue",
+		"Orange",
+		"Green",
+		"Brown",
+		"Slate"
+	};
+
+	CColorPair::CColorPair(const MajorColor &major, const MinorColor &minor) :
+		m_majorColor(major),
+		m_minorColor(minor)
+	{
+	}
+
+    MajorColor CColorPair::getMajor()
+    {
+        return m_majorColor;
+    }
+
+    MinorColor CColorPair::getMinor()
+    {
+        return m_minorColor;
+    }
+
+    std::string CColorPair::ToString()
+    {
+		return (std::string(MajorColorNames[m_majorColor]) + std::string(" ") + std::string(MinorColorNames[m_minorColor]));
+    }
+}
