27c50cf2d473406adb06ecffa4078421e338bb78
Avinash Bhaskar Bhat <AvinashBhaskar.Bhat@philips.com>
2024-07-29 17:26:55 +0530

    Added enums for major and minor colors

---
+++ ColorConstants.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace TelCoColorCoder
+{
+	enum MajorColor
+	{
+		WHITE,
+		RED,
+		BLACK,
+		YELLOW,
+		VIOLET,
+
+		MAX_MAJOR_COLOR
+	};
+
+	enum MinorColor
+	{
+		BLUE,
+		ORANGE,
+		GREEN,
+		BROWN,
+		SLATE,
+
+		MAX_MINOR_COLOR
+	};
+}
